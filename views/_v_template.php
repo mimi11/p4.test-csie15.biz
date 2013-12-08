6ca9cee4257768e2e079782872f99975c533ead9
Carine <cmelhorn@g.harvard.edu>
2013-12-08 17:04:48 -0500

    core framework set-up work

---
+++ views/_v_template.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php if(isset($title)) echo $title; ?></title>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
+					
+	<!-- Controller Specific JS/CSS -->
+	<?php if(isset($client_files_head)) echo $client_files_head; ?>
+    <link rel="stylesheet" href="/css/sample-app.css" type ="text/css"/>
+
+
+</head>
+
+<body>
+
+
+    <div id='menu'>
+
+        <a href='/'>Home</a>
+
+        <!-- Menu for users who are logged in -->
+        <?php if($user): ?>
+
+            <a href='/users/logout'>Logout</a>
+            <a href='/users/profile'>Profile</a>
+
+            <!-- Menu options for users who are not logged in -->
+        <?php else: ?>
+
+            <a href='/users/signup'>Sign up</a>
+            <a href='/users/login'>Log in</a>
+
+        <?php endif; ?>
+        <?php if(isset($content)) echo $content; ?>
+        <?php if(isset($client_files_body)) echo $client_files_body; ?>
+
+    </div>
+
+</body>
+</html>
